727924c576d3c93c15751911e8f9f04a6e2618cc
Kat Matfield <katmatfield@gmail.com>
2021-09-04 19:19:33 +0200

    Show one right answer and a question

--- app/models.py
+++ app/models.py
@@ -7,10 +7,52 @@ import datetime
 from app import app, db
 
 
+class VerbStem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stem = db.Column(db.Text)
+
+
+class VerbPrefix(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prefix = db.Column(db.Text)
+
+
+class VerbMeaning(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    meaning = db.Column(db.Text)
+    hint = db.Column(db.Text)
+
+
+class Combination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prefix_id = db.Column(db.Integer, db.ForeignKey('verb_prefix.id'))
+    stem_id = db.Column(db.Integer, db.ForeignKey('verb_stem.id'))
+    meaning_id = db.Column(db.Integer, db.ForeignKey('verb_meaning.id'))
+
+    def get_learning_info(self, question_type):
+        prefix = VerbPrefix.query.filter_by(id=self.prefix_id).first()
+        stem = VerbStem.query.filter_by(id=self.stem_id).first()
+        meaning = VerbMeaning.query.filter_by(
+                            id=self.meaning_id).first()
+        if meaning.hint:
+            meaning = meaning.meaning + " " + meaning.hint
+        else:
+            meaning = meaning.meaning
+        if question_type == "different_prefix":
+            return {"question_type": question_type,
+                    "combination_id": self.id,
+                    "right_answer": prefix.prefix+stem.stem,
+                    "question": meaning
+                    }
+
+
 class Learner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     last_seen = db.Column(db.Date)
+    learning_histories = db.relationship('LearningHistory',
+                                         backref='learner',
+                                         lazy='dynamic')
 
     @classmethod
     def create_or_find(cls, submitted_name):
@@ -26,3 +68,29 @@ class Learner(db.Model):
             db.session.add(new_learner)
             db.session.commit()
             return new_learner
+
+
+class LearningHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    combination_id = db.Column(db.Integer, db.ForeignKey('combination.id'))
+    learner_id = db.Column(db.Integer, db.ForeignKey('learner.id'))
+    direction = db.Column(db.Text)
+    last_correct_date = db.Column(db.Date)
+    ease = db.Column(db.Integer)
+
+    @classmethod
+    def get_question(cls, learner_id):
+        question_type = random.choice([
+                                       "different_prefix",
+                                       # "different_stem"
+                                       ])
+        next_question = cls.query.filter_by(learner_id=learner_id).filter(
+                            cls.last_correct_date != datetime.datetime.today()
+                            ).first()
+        if next_question:
+            combination = Combination.query.filter_by(
+                       next_question.first().combination_id).first()
+            return combination.get_learning_info(question_type)
+        else:
+            combination = random.choice(Combination.query.all())
+            return combination.get_learning_info(question_type)
--- app/views.py
+++ app/views.py
@@ -11,6 +11,18 @@ def home():
     form = NameForm()
     if request.method == "POST" and form.validate_on_submit():
         learner = Learner.create_or_find(form.name.data)
-        return render_template('home.html', learner=learner)
+        return redirect('learn/{0}'.format(learner.id))
     else:
         return render_template('home.html', form=form)
+
+
+@app.route('/learn/<learner_id>', methods=['GET', 'POST'])
+def learn(learner_id):
+    if request.method == "GET":
+        next_question = LearningHistory.get_question(learner_id)
+        return render_template('learn.html', 
+                               combination=next_question["combination_id"],
+                               right_answer=next_question["right_answer"],
+                               question=next_question["question"],
+                               question_type=next_question["question_type"]
+                               )
