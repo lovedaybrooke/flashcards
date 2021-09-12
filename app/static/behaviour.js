51b2349e2935e8038f5c64c0926acfd6e94d57fa
Kat Matfield <katmatfield@gmail.com>
2021-09-12 14:22:03 +0200

    Show hints more nicely on answer cards

--- app/static/behaviour.js
+++ app/static/behaviour.js
@@ -1,9 +1,11 @@
 function respond() {
 
+  var result = event.srcElement.closest(".answer-card").id
+
   var buttonDiv = document.getElementsByClassName("slanted-button");
   buttonDiv[0].style.display = 'block';
 
-  var answerCard = document.getElementsByClassName("answer-card").namedItem(event.srcElement.id);
+  var answerCard = document.getElementsByClassName("answer-card").namedItem(result);
   answerCard.classList.remove("answer-concealed");
 
   const xhr = new XMLHttpRequest();
@@ -11,7 +13,7 @@ function respond() {
   var FD  = new FormData();
   FD.append( "question_type", document.getElementById("question-type").innerText );
   FD.append( "combination_id", document.getElementById("combination").innerText );
-  FD.append( "result", event.srcElement.id );
+  FD.append( "result", result );
 
   xhr.open('POST', postURL);
   xhr.send( FD );  
